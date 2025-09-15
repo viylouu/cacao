55db6effe3c0b040d3a328c4835ecb41c34ea412
viylouu <131220600+viylouu@users.noreply.github.com>
2025-09-14 20:58:12 -0600

    cleanup

--- core/platform/platform_wl.c
+++ core/platform/platform_wl.c
@@ -16,23 +16,24 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
-struct wl_display* wldisp;
-struct wl_registry* wlreg;
-struct xdg_surface* xdgsurf;
+struct wl_display* waylandDisplay;
+struct wl_registry* waylandRegistry;
+struct xdg_surface* xdgSurface;
+struct wl_callback* waylandCallback;
 
-struct wl_compositor* wlcomp;
-struct wl_surface* wlsurf;
-struct wl_buffer* wlbuf;
-struct wl_shm* wlshm; //shared memory
-struct xdg_wm_base* xdgshell;
-struct xdg_toplevel* xdgtop;
-uint8_t* wlpix;
+struct wl_compositor* waylandCompositor;
+struct wl_surface* waylandSurface;
+struct wl_buffer* waylandBuffer;
+struct wl_shm* waylandSharedMemory; //shared memory
+struct xdg_wm_base* xdgShell;
+struct xdg_toplevel* xdgTopLevel;
+uint8_t* waylandPixel;
 
 uint16_t width = 200;
 uint16_t height = 100;
 
 // allocate shared memory
-int32_t wlAllocShm(uint64_t size) {
+int32_t wlAllocSharedMemory(uint64_t size) {
     int8_t name[8];
     name[0] = '/';
     name[7] = 0;
@@ -47,67 +48,81 @@ int32_t wlAllocShm(uint64_t size) {
 }
 
 void wlResize() {
-    int32_t filedesc = wlAllocShm(width * height * 4); // width * height, * 4 because each pix is 4 bytes (rgba)
+    int32_t filedesc = wlAllocSharedMemory(width * height * 4); // width * height, * 4 because each pix is 4 bytes (rgba)
     
-    wlpix = mmap(0, width*height*4, PROT_READ | PROT_WRITE, MAP_SHARED, filedesc, 0);
+    waylandPixel = mmap(0, width*height*4, PROT_READ | PROT_WRITE, MAP_SHARED, filedesc, 0);
 
-    struct wl_shm_pool* pool = wl_shm_create_pool(wlshm, filedesc, width*height*4);
+    struct wl_shm_pool* pool = wl_shm_create_pool(waylandSharedMemory, filedesc, width*height*4);
 
-    wlbuf = wl_shm_pool_create_buffer(pool, 0, width,height, width*4, WL_SHM_FORMAT_ARGB8888);
+    waylandBuffer = wl_shm_pool_create_buffer(pool, 0, width,height, width*4, WL_SHM_FORMAT_ARGB8888);
     
     wl_shm_pool_destroy(pool);
     close(filedesc);
 }
 
 void draw() {
-    
+    memset(waylandPixel, 255, width*height*4);
+
+    wl_surface_attach(waylandSurface, waylandBuffer, 0,0);
+    wl_surface_damage_buffer(waylandSurface, 0,0, width,height);
+    wl_surface_commit(waylandSurface);
 }
 
-void xdgsurfConf(void* data, struct xdg_surface* xdgsurf, uint32_t serial) {
-    xdg_surface_ack_configure(xdgsurf, serial);
-    if (!wlpix)
+struct wl_callback_listener waylandCallbackListener;
+
+void newFrame(void* data, struct wl_callback* waylandCallback, uint32_t callbackData) {
+    wl_callback_destroy(waylandCallback);
+    waylandCallback = wl_surface_frame(waylandSurface);
+    wl_callback_add_listener(waylandCallback, &waylandCallbackListener, 0);
+
+    draw();
+}
+
+struct wl_callback_listener waylandCallbackListener = {
+    .done = newFrame
+};
+
+void xdgSurfaceConf(void* data, struct xdg_surface* xdgSurface, uint32_t serial) {
+    xdg_surface_ack_configure(xdgSurface, serial);
+    if (!waylandPixel)
         wlResize();
 
     draw();
-
-    wl_surface_attach(wlsurf, wlbuf, 0,0);
-    wl_surface_damage_buffer(wlsurf, 0,0, width,height);
-    wl_surface_commit(wlsurf);
 }
 
-struct xdg_surface_listener xdgsurflist = {
-    .configure = xdgsurfConf
+struct xdg_surface_listener xdgSurfaceListener = {
+    .configure = xdgSurfaceConf
 };
 
-void xdgtopConf(void* data, struct xdg_toplevel* xdgtop, int32_t width, int32_t height, struct wl_array* wlstates) {
+void xdgTopLevelConf(void* data, struct xdg_toplevel* xdgTopLevel, int32_t width, int32_t height, struct wl_array* wlstates) {
     
 }
 
-void xdgtopClose(void* data, struct xdg_toplevel* xdgtop) {
+void xdgTopLevelClose(void* data, struct xdg_toplevel* xdgTopLevel) {
 
 }
 
-struct xdg_toplevel_listener xdgtoplist = {
-    .configure = xdgtopConf,
-    .close = xdgtopClose
+struct xdg_toplevel_listener xdgTopLevelListener = {
+    .configure = xdgTopLevelConf,
+    .close = xdgTopLevelClose
 };
 
-void xdgshellPing(void* data, struct xdg_wm_base* xdgshell, uint32_t serial) {
-    xdg_wm_base_pong(xdgshell, serial);
+void xdgShellPing(void* data, struct xdg_wm_base* xdgShell, uint32_t serial) {
+    xdg_wm_base_pong(xdgShell, serial);
 }
 
-struct xdg_wm_base_listener xdgshelllist = {
-    .ping = xdgshellPing
+struct xdg_wm_base_listener xdgShellListener = {
+    .ping = xdgShellPing
 };
 
 void wlRegGlob(void* data, struct wl_registry* reg, uint32_t name, const char* intf, uint32_t ver) {
     if (!strcmp(intf, wl_compositor_interface.name))
-        wlcomp = wl_registry_bind(wlreg, name, &wl_compositor_interface, 4);
+        waylandCompositor = wl_registry_bind(waylandRegistry, name, &wl_compositor_interface, 4);
     else if(!strcmp(intf, wl_shm_interface.name))
-        wlshm = wl_registry_bind(wlreg, name, &wl_shm_interface, 1);
+        waylandSharedMemory = wl_registry_bind(waylandRegistry, name, &wl_shm_interface, 1);
     else if(!strcmp(intf, xdg_wm_base_interface.name)) {
-        xdgshell = wl_registry_bind(wlreg, name, &xdg_wm_base_interface, 1);
-        xdg_wm_base_add_listener(xdgshell, &xdgshelllist, 0);
+        xdgShell = wl_registry_bind(waylandRegistry, name, &xdg_wm_base_interface, 1);
+        xdg_wm_base_add_listener(xdgShell, &xdgShellListener, 0);
     }
 }
 
@@ -115,7 +130,7 @@ void wlRegGlobRemove(void* data, struct wl_registry* reg, uint32_t name) {
     
 }
 
-struct wl_registry_listener wlreglist = {
+struct wl_registry_listener waylandRegistryListener = {
     .global = wlRegGlob,
     .global_remove = wlRegGlobRemove
 };
@@ -124,34 +139,39 @@ int8_t wlPlatformInit(const char* title, int32_t targwidth, int32_t targheight) 
     width = targwidth;
     height = targheight;
 
-    wldisp = wl_display_connect(0);
-    wlreg = wl_display_get_registry(wldisp);
+    waylandDisplay = wl_display_connect(0);
+    waylandRegistry = wl_display_get_registry(waylandDisplay);
 
-    wl_registry_add_listener(wlreg, &wlreglist, 0);
-    wl_display_roundtrip(wldisp);
+    wl_registry_add_listener(waylandRegistry, &waylandRegistryListener, 0);
+    wl_display_roundtrip(waylandDisplay);
 
-    wlsurf = wl_compositor_create_surface(wlcomp);
+    waylandSurface = wl_compositor_create_surface(waylandCompositor);
+    waylandCallback = wl_surface_frame(waylandSurface);
+    wl_callback_add_listener(waylandCallback, &waylandCallbackListener, 0);
 
-    xdgsurf = xdg_wm_base_get_xdg_surface(xdgshell, wlsurf);
-    xdg_surface_add_listener(xdgsurf, &xdgsurflist, 0);
+    xdgSurface = xdg_wm_base_get_xdg_surface(xdgShell, waylandSurface);
+    xdg_surface_add_listener(xdgSurface, &xdgSurfaceListener, 0);
 
-    xdgtop = xdg_surface_get_toplevel(xdgsurf);
-    xdg_toplevel_add_listener(xdgtop, &xdgtoplist, 0);
+    xdgTopLevel = xdg_surface_get_toplevel(xdgSurface);
+    xdg_toplevel_add_listener(xdgTopLevel, &xdgTopLevelListener, 0);
 
-    xdg_toplevel_set_title(xdgtop, title);
+    xdg_toplevel_set_title(xdgTopLevel, title);
 
-    wl_surface_commit(wlsurf);
+    wl_surface_commit(waylandSurface);
 
-    while(wl_display_dispatch(wldisp));
+    while(wl_display_dispatch(waylandDisplay));
 
     return 0;
 }
 
 int8_t wlPlatformDeinit() {
-    if (wlbuf)
-        wl_buffer_destroy(wlbuf);
+    if (waylandBuffer)
+        wl_buffer_destroy(waylandBuffer);
 
-    wl_surface_destroy(wlsurf);
-    wl_display_disconnect(wldisp);
+    xdg_toplevel_destroy(xdgTopLevel);
+    xdg_surface_destroy(xdgSurface);
+
+    wl_surface_destroy(waylandSurface);
+    wl_display_disconnect(waylandDisplay);
     return 0;
 }
