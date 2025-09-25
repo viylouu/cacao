713ec73aa5bd1ba5447954c0c281d9469c8e47f5
viylouu <131220600+viylouu@users.noreply.github.com>
2025-09-24 21:41:32 -0600

    basic collision

---
+++ collision/collision.h
@@ -0,0 +1,10 @@
+#ifndef CC_COLLISION_H
+#define CC_COLLISION_H
+
+#include <core/macros/macros.h>
+
+b8 cc_collidePointRect(f32 x, f32 y, f32 rx, f32 ry, f32 rw, f32 rh) {
+    return x >= rx && y >= ry && x <= rx+rw && y <= ry+rh;
+}
+
+#endif
