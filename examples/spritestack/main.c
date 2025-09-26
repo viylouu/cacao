83f6355359f0cfc6a042e0581c9ad34080972264
viylouu_the_second <29hunter.shucart@boiseschools.net>
2025-09-26 08:00:53 -0600

    using new api

--- examples/spritestack/main.c
+++ examples/spritestack/main.c
@@ -1,19 +1,10 @@
 #include <core/engine.h>
 #include <core/renderer/renderer.h>
 
-typedef struct {
-    CCtexture* texture;
-    u32 layers;
-    u32 layerHeight;
-} CCspriteStack;
-
 CCspriteStack* stack;
 
 void init(void) {
-    stack = malloc(sizeof(CCspriteStack));
-    stack->texture = cc_loadTexture("examples/spritestack/car.png");
-    stack->layerHeight = 13;
-    stack->layers = stack->texture->height / stack->layerHeight;
+    stack = cc_loadSpriteStack("examples/spritestack/car.png", 13);
 }
 
 void update(void) {}
@@ -23,30 +14,11 @@ void render(void) {
 
     cc_rendererSetTint(1,1,1,1);
 
-    f32 x, y, scale;
-    x = cc_width / 2;
-    y = cc_height / 2;
-    scale = 8;
-
-    cc_rendererScale(scale,scale,1);
-
-    mat4 prev;
-    cc_rendererGetTransform(&prev);
-
-    for (f32 i = stack->layers-1; i >= 0; i -= 1.f/scale) {
-        cc_rendererRotate(0,0,45*(3.14159256f/180.f));
-
-        cc_rendererTranslate(x, y, 0);
-        cc_rendererTranslate(0,i*scale,0);
-
-        cc_rendererDrawTexture(stack->texture, 0,0,stack->texture->width,stack->layerHeight, 0,(s32)i*stack->layerHeight,stack->texture->width,stack->layerHeight);
-        cc_rendererSetTransform(&prev);
-    }
+    cc_rendererDrawSpriteStack(stack, cc_width/2, cc_height/2, 0, 8, 45*(3.14159265f/180.f));
 }
 
 void clean(void) {
-    cc_unloadTexture(stack->texture);
-    free(stack);
+    cc_unloadSpriteStack(stack);
 }
 
 int main(void) {
