020b30bf638d52f559c3291debf0505c606aeaa9
viylouu <131220600+viylouu@users.noreply.github.com>
2025-09-27 15:13:42 -0600

    name

--- nodes/node.c
+++ nodes/node.c
@@ -6,7 +6,9 @@ u32 cc_scene_amt;
 CCnode cc_root;
 
 void cc_nodesInit(void) {
-    cc_scenes = malloc(sizeof(CCnode));    
+    cc_scenes = malloc(sizeof(CCnode));  
+
+    cc_root.name = "root";
 }
 
 void cc_nodesUninit(void) {
