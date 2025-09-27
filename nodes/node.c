ef14cfa96ee82d44a1f06f0c9557b85a037db521
viylouu <131220600+viylouu@users.noreply.github.com>
2025-09-27 15:19:37 -0600

    no more segfault >:(

--- nodes/node.c
+++ nodes/node.c
@@ -9,6 +9,11 @@ void cc_nodesInit(void) {
     cc_scenes = malloc(sizeof(CCnode));  
 
     cc_root.name = "root";
+
+    cc_root.child_amt = 0;
+    cc_root.child_capac = 0;
+    cc_root.component_amt = 0;
+    cc_root.component_capac = 0;
 }
 
 void cc_nodesUninit(void) {
